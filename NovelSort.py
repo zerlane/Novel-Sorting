9e52a2f661eb0c868e30305b2a693fefbf5e452f
zerlane <nautishayc@gmail.com>
2022-07-12 22:27:38 -0500

    now reads in file and sorts txt file

--- NovelSort.py
+++ NovelSort.py
@@ -23,21 +23,34 @@ def findMinMax(arr, left, right):
 
     return minIndex, maxIndex
 
-arr = [8, 5, 2, 13, 11]
-print(novelSort(arr, 0, len(arr) - 1))
+# arr = [8, 5, 2, 13, 11]
+# print(novelSort(arr, 0, len(arr) - 1))
 
 
+#read in file
+novelSortFile = open("/Users/zerlane/Documents/UAB Summer22/CS303/Labs/Lab5/NovelSortInput.txt", "r")
 
-with open("/Users/zerlane/Documents/UAB Summer22/CS303/Lab5/NovelSortInput.txt") as f:
-    for line in f:
-        if len(line) <= 1:
-            line.remove(" ")
+inElements = novelSortFile.read()
 
+elementsIntoList = inElements.split("\n\n")
+
+novelSortFile.close()
+print(elementsIntoList)
+# convert string to time (https://www.educative.io/answers/how-to-convert-a-string-to-a-date-in-python)
 
 #sort method to sort list alphabettically
-# for i in range (0, len(arr)):
-#     for j in range(0, len(arr)):
-#         if(arr[j] > arr[i]):
-#             temp = arr[i];
-#             arr[i] = arr[j];
-#             arr[j] = temp;
+def alphabetize(arr):
+    for i in range (0, len(arr)):
+        for j in range(0, len(arr)):
+            if(arr[j] > arr[i]):
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+    return arr
+
+print(alphabetize(elementsIntoList))
+
+#bucket sort
+
+def bucketSort (arr, n):
+    return arr
